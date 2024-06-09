3e23a50107084922e5b648c09c2837b8364bd152
s33chin <shane.chin97@yahoo.com>
2024-06-09 00:06:51 -0500

    added "Look up" API

--- frontend/src/components/Results/Result/Result.js
+++ frontend/src/components/Results/Result/Result.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './Result.css';
 
 export default function Result(props) {
-    return(
-    <div className="card result">
+    const maxLength = 50;
+    const content = props.document.content.length > maxLength ? props.document.content.substring(0, maxLength) + '...' : props.document.content;
+
+    return (
+        <div className="card result">
+            <img className="card-img-top" src={'/images/research-paper.png'} alt="Place Holder"></img>
             <div className="card-body">
-                <h6 className="title-style">{props.document.content}</h6>
+                <h6 className="title-style">{content}</h6>
             </div>
-    </div>
+        </div>
     );
 }
