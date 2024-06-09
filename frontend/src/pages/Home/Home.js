ece57b2273704d04f6625d07193365a2c389d95c
s33chin <shane.chin97@yahoo.com>
2024-06-09 01:02:59 -0500

    minor changes homepage

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -13,27 +13,18 @@ export default function Home() {
         history.push('/search?q=' + q);
     }
 
-    const datasets = [
-        { name: "Department of Justice (DOJ)", image: "/images/justice.png" },
-        { name: "Department of Housing and Urban Development (HUD)", image: "/images/Housing_and_Urban.png" },
-        { name: "Department of Education", image: "/images/education.png" },
-        { name: "Police Departments", image: "/images/police-department-logo.png" }
-    ];
-
     return (
         <main className="main main--home">
             <div className="row home-search">
                 <img className="logo" src="/images/cognitive-search.png" alt="Cognitive Search"></img>
                 <p className="poweredby lead">Powered by Azure Cognitive Search</p>
                 <SearchBar postSearchHandler={navigateToSearchPage}></SearchBar>
-            </div>
-            <div className="row datasets">
-                {datasets.map((dataset, index) => (
-                    <div key={index} className="dataset">
-                        <img src={dataset.image} alt={dataset.name} />
-                        <p>{dataset.name}</p>
-                    </div>
-                ))}
+                <h2>Available Goverment Datasets</h2>
+                <div className="image-container">
+                    <img src="/images/justice.png" alt="Image 1" />
+                    <img src="/images/Housing_and_Urban.png" alt="Image 2" />
+                    <img src="/images/education.png" alt="Image 3" />
+                </div>
             </div>
         </main>
     );
