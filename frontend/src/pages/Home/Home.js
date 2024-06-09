49aa44fdfd4198a5abf0cc2550abf556282b6270
s33chin <shane.chin97@yahoo.com>
2024-06-09 00:45:14 -0500

    changes home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-
 import SearchBar from '../../components/SearchBar/SearchBar';
-
 import "./Home.css";
 import "../../pages/Search/Search.css";
 
@@ -15,12 +13,27 @@ export default function Home() {
         history.push('/search?q=' + q);
     }
 
+    const datasets = [
+        { name: "Department of Justice (DOJ)", image: "/images/justice.png" },
+        { name: "Department of Housing and Urban Development (HUD)", image: "/images/Housing_and_Urban.png" },
+        { name: "Department of Education", image: "/images/education.png" },
+        { name: "Police Departments", image: "/images/police-department-logo.png" }
+    ];
+
     return (
         <main className="main main--home">
             <div className="row home-search">
                 <img className="logo" src="/images/cognitive-search.png" alt="Cognitive Search"></img>
                 <p className="poweredby lead">Powered by Azure Cognitive Search</p>
                 <SearchBar postSearchHandler={navigateToSearchPage}></SearchBar>
+            </div>
+            <div className="row datasets">
+                {datasets.map((dataset, index) => (
+                    <div key={index} className="dataset">
+                        <img src={dataset.image} alt={dataset.name} />
+                        <p>{dataset.name}</p>
+                    </div>
+                ))}
             </div>
         </main>
     );
